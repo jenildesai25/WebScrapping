0a4649e78880a31fc89b1cb2bd10affa666d99fd
jenildesai25 <jenil.desai25@gmail.com>
2018-11-29 19:59:53 -0600

    updated code for python 3.6

--- WebScrapping.py
+++ WebScrapping.py
@@ -1,20 +1,21 @@
 from bs4 import BeautifulSoup
-import urllib2
+import requests
 
 if __name__ == '__main__':
-    user_input = raw_input('What is the year that you are interested in getting data:')
-    page = 'https://www.boxofficemojo.com/yearly/chart/?yr=' + user_input + '&p=.htm%20'
-    page_open = urllib2.urlopen(page)
-    soup = BeautifulSoup(page_open, 'html.parser')
-    # print(soup)
+    user_input = input('What is the year that you are interested in getting data:')
+    url = 'https://www.boxofficemojo.com/yearly/chart/?yr=' + user_input + '&p=.htm%20'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # print('page data is:', soup)
     # the data we needed is after <b> so we can find it by find_all function
     scrapped_data = []
     # data comes in unicode you need to convert to string.encode will help you in that.
     for movie_name_and_total_gross in soup.find_all('b'):
         # data comes in unicode you need to convert to string.encode will help you in that.
-        scrapped_data.append(movie_name_and_total_gross.text.encode('UTF8'))
-    # print(scrapped_data)
+        scrapped_data.append(movie_name_and_total_gross.text)
+    print(scrapped_data)
     ids_of_movie = []
-    for ids in soup.find_all('a'):
-        ids_of_movie.append(ids.text.encode('UTF8'))
+    for ids in soup.find_all('a', href=lambda href: href and "movies" in href):
+        # print(ids['href'])
+        ids_of_movie.append(ids['href'])
     print(ids_of_movie)
