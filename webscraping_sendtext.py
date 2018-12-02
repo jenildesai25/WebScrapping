d0e93016d95e44968f4df3b1ea328fe61a8b0b63
Virti Bipin Sanghavi <virtibipin.sanghavi@mavs.uta.edu>
2018-12-01 21:04:10 -0600

    new_code_jenil_page_change

--- webscraping_sendtext.py
+++ webscraping_sendtext.py
@@ -33,10 +33,11 @@ import os
 import glob
 import re
 
-print("Welcome to Internet Movie Scraper Database (IMSDB). Please specify a year, and we will create a text document that"
-      "will tell you all about the movie business that year.")
+print(
+    "Welcome to Internet Movie Scraper Database (IMSDB). Please specify a year, and we will create a text document that"
+    "will tell you all about the movie business that year.")
 
-file_path = "C:/Users/Virti Sanghavi/Pictures/boxofficemojo" #Where you want to write the file to. Make sure to specify the location, as stated above.
+file_path = "C:/Users/Virti Sanghavi/Pictures/boxofficemojo"  # Where you want to write the file to. Make sure to specify the location, as stated above.
 
 if os.path.exists(str(file_path)) is False:
     os.mkdir(str(file_path))
@@ -48,7 +49,8 @@ if len(glob.glob("*")) != 0:
     for file in file_list:
         os.remove(file)
 
-year = input("Please enter a year as a valid integer between the year 2018 and 1980: ") # Need to add in try/except blocks here.
+year = input(
+    "Please enter a year as a valid integer between the year 2018 and 1980: ")  # Need to add in try/except blocks here.
 
 try:
     year = int(year)
@@ -64,18 +66,31 @@ if not 1980 <= year <= 2018:
 # pulling a range is a little sloppy. I could find the total number of pages and use that as the range--there were never
 # more than 1000 movies released any year (or at least that Box Office Mojo tracks. One could find the actual number of
 # pages scraped. But I didn't do that. Maybe you can.
-
-for num in range(1,10,1):
+url = 'https://www.boxofficemojo.com/yearly/chart/?page={}&view=releasedate&view2=domestic&yr={}&p=.htm'.format(
+    1, year)
+pages_data = []
+total_pages = []
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+for page in soup.find_all('a', href=lambda href: href and "page" in href):
+    pages_data.append(page['href'])
+for page in pages_data:
+    if 'page' in page:
+        index = page.find('page')
+        if page[index:index + 6] not in total_pages:
+            total_pages.append(page[index:index + 6])
+for num in range(1, len(total_pages) + 1, 1):
     try:
-        url = 'https://www.boxofficemojo.com/yearly/chart/?page={}&view=releasedate&view2=domestic&yr={}&p=.htm'.format(num, year)  # This one works
+        url = 'https://www.boxofficemojo.com/yearly/chart/?page={}&view=releasedate&view2=domestic&yr={}&p=.htm'.format(
+            num, year)  # This one works
         print("Grabbing page {} for {}".format(num, year))
         r = requests.get(url)
         html = r.text
         soup = BeautifulSoup(html, 'lxml')
-        table = soup.find('table', {"cellspacing":"1"})
+        table = soup.find('table', {"cellspacing": "1"})
         df = pd.read_html(str(table), skiprows=2)
         df = df[0]
-        df = df.iloc[:, :7] #This slices the dataframe to cut off the date sections.
+        df = df.iloc[:, :7]  # This slices the dataframe to cut off the date sections.
         df.columns = ['rank', 'title', 'studio', 'total gross', 'total theaters', 'opening gross', 'opening theaters']
         df = df[:len(df.index) - 3]
         df = df[['title', 'studio', 'total gross', 'total theaters', 'opening gross', 'opening theaters']]
@@ -93,7 +108,8 @@ for num in range(1,10,1):
 
         id_list = [x.split("=")[1] for x in id_list]  # isolating the id 1
         id_list = [x.split(".")[0] for x in id_list]  # isolating the id 2
-        id_list = id_list[1:]  # cutting off the first entry (first entry gives the #1 box office entry for the current week).
+        id_list = id_list[
+                  1:]  # cutting off the first entry (first entry gives the #1 box office entry for the current week).
         id_dict = dict(zip(title_list, id_list))
 
         for index in df.index:
@@ -122,18 +138,19 @@ for file in file_list:
 df = pd.read_csv("movies.txt", sep="\t")
 
 # Clean the data, else it won't be able to get the mean of the total gross.
-df['total gross'] = df['total gross'].apply(lambda x: x.replace('$', '')) # replace dollar signs
-df['total gross'] = df['total gross'].apply(lambda x: x.replace(',', '')) # replace commas
-df['total gross'] = df['total gross'].apply(lambda x: int(x)) # turn it from a str to int
+df['total gross'] = df['total gross'].apply(lambda x: x.replace('$', ''))  # replace dollar signs
+df['total gross'] = df['total gross'].apply(lambda x: x.replace(',', ''))  # replace commas
+df['total gross'] = df['total gross'].apply(lambda x: int(x))  # turn it from a str to int
 
 # Dollar formatting. :) See here: https://stackoverflow.com/questions/21208376/converting-float-to-dollars-and-cents
 average_gross_earnings = '${:,.2f}'.format(df['total gross'].mean())
 
-df = df.sort_values(by='total theaters', ascending=False) # sorts the DataFrame to get the highest theater number.
+df = df.sort_values(by='total theaters', ascending=False)  # sorts the DataFrame to get the highest theater number.
 
 movie_most_theaters = df.iloc[0]['title']
 most_theaters_total = int(df.iloc[0]['total theaters'])
 
 print("---------------------------")
 print("The average total gross earnings for the year {} was {}".format(year, average_gross_earnings))
-print("The movie that was shown in the most theaters was '{}' with {} theaters".format(movie_most_theaters, most_theaters_total))
+print("The movie that was shown in the most theaters was '{}' with {} theaters".format(movie_most_theaters,
+                                                                                       most_theaters_total))
